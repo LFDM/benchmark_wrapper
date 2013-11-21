d13dbc48f1ca0b3085c05e35b2d7ff7375070855
LFDM <1986gh@gmail.com>
2013-11-21 16:37:18 +0100

    Uses keyword arguments in Wrapper.

--- lib/benchmark_wrapper/wrapper.rb
+++ lib/benchmark_wrapper/wrapper.rb
@@ -2,10 +2,10 @@ module BenchmarkWrapper
   class Wrapper
     attr_reader :wrapped_method, :out, :out_method
 
-    def initialize(meth, options = { out: $stdout, out_method: :puts })
+    def initialize(meth, out: $stdout, out_method: :puts)
       @wrapped_method = meth
-      @out        = options[:out]
-      @out_method = options[:out_method]
+      @out        = out
+      @out_method = out_method
     end
   end
 end
