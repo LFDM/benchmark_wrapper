6162246fcef3a3eb042732bc2b02e9482fa50975
LFDM <1986gh@gmail.com>
2013-11-21 16:34:43 +0100

    Adds to Wrapper object.

--- lib/benchmark_wrapper/wrapper.rb
+++ lib/benchmark_wrapper/wrapper.rb
@@ -1,10 +1,11 @@
 module BenchmarkWrapper
   class Wrapper
-    attr_reader :method, :out, :out_method
+    attr_reader :wrapped_method, :out, :out_method
 
-    def initialize(meth)
-      @method = meth
-
+    def initialize(meth, options = { out: $stdout, out_method: :puts })
+      @wrapped_method = meth
+      @out        = options[:out]
+      @out_method = options[:out_method]
     end
   end
 end
--- spec/lib/benchmark_wrapper/wrap_spec.rb
+++ spec/lib/benchmark_wrapper/wrap_spec.rb
@@ -5,8 +5,37 @@ describe BenchmarkWrapper::Wrapper  do
 
   describe "#wrapped_method" do
     it "returns the method new the object has been initialized with" do
-      wrapper.new(:a_method)
-      wrapper.method.should == :a_method
+      wrap = wrapper.new(:a_method)
+      wrap.wrapped_method.should == :a_method
+    end
+  end
+
+  describe "#out" do
+    describe "returns the object that's used for outputting the bm result" do
+      it "it set through an options hash upon initialize" do
+        stub = double
+        wrap = wrapper.new(:a_method, out: stub)
+        wrap.out.should == stub
+      end
+
+      it "defaults to $stdout" do
+        wrap = wrapper.new(:a_method)
+        wrap.out.should == $stdout
+      end
+    end
+  end
+
+  describe "#out_method" do
+    describe "returns the method that's used for outputting the bm result" do
+      it "it set through an options hash upon initialize" do
+        wrap = wrapper.new(:a_method, out_method: :b_method)
+        wrap.out_method.should == :b_method
+      end
+
+      it "defaults to #puts" do
+        wrap = wrapper.new(:a_method)
+        wrap.out_method.should == :puts
+      end
     end
   end
 end
