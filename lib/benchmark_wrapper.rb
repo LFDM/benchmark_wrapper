4b568a0545192fe28842015ec28a25f15df203d5
LFDM <1986gh@gmail.com>
2013-11-21 16:23:18 +0100

    Adds register method.

--- lib/benchmark_wrapper.rb
+++ lib/benchmark_wrapper.rb
@@ -1,5 +1,12 @@
 require "benchmark_wrapper/version"
 
 module BenchmarkWrapper
-  # Your code goes here...
+  require "benchmark_wrapper/wrapper"
+
+  def wrap_with_benchmark(*registered_methods)
+    @to_be_wrapped_with_benchmark ||= []
+    registered_methods.each do |meth|
+      @to_be_wrapped_with_benchmark << meth
+    end
+  end
 end
--- spec/lib/benchmark_wrapper_spec.rb
+++ spec/lib/benchmark_wrapper_spec.rb
@@ -1,11 +1,46 @@
 require 'spec_helper'
+require 'stringio'
 
 describe BenchmarkWrapper do
+  before :all do
+    string_io = StringIO.new
+    @stdout = $stdout
+    $stdout = string_io
+  end
+
+  let(:dummy) do
+    Class.new do
+      extend BenchmarkWrapper
+
+      def test1
+        1 + 1
+      end
+
+      def test2
+        2 + 2
+      end
+    end
+  end
+
   it 'should have a version number' do
     BenchmarkWrapper::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  describe "#wrap_with_benchmark" do
+    it "registers a given method" do
+      dummy.wrap_with_benchmark(:a_method)
+      container = dummy.instance_variable_get(:@to_be_wrapped_with_benchmark)
+      container.should have(1).item
+    end
+
+    it "takes several methods at once" do
+      dummy.wrap_with_benchmark(:a_method, :other_method)
+      container = dummy.instance_variable_get(:@to_be_wrapped_with_benchmark)
+      container.should have(2).items
+    end
+  end
+
+  after :all do
+    $stdout = @stdout
   end
 end
